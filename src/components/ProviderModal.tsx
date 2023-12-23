7ffcec8e04b04a7b2d724bc5e649a6cf7732ecf1
Ivan Vityaev <godofluck89@gmail.com>
2023-12-23 19:07:32 +0300

    add providers

--- src/components/ProviderModal.tsx
+++ src/components/ProviderModal.tsx
@@ -1,4 +1,6 @@
-import { Modal } from "antd";
+import { Form, Input, Modal, Select } from "antd";
+import * as ccxt from 'ccxt'
+import React from "react";
 
 interface IProps {
 	visible: boolean;
@@ -7,9 +9,31 @@ interface IProps {
 }
 
 export function ProviderModal (props: IProps) {
+
+	const providers = React.useMemo(() => {
+		return Object.keys(ccxt.exchanges);
+	}, []);
+
 	return (
 		<Modal open={props.visible} title="Provider" onCancel={props.toggle}>
-
+			<Form>
+				<Form.Item name="provider" label="Provider">
+					<Select showSearch placeholder="Not Selected">
+						{providers.map(p => {
+							return <Select.Option key={p} value={p}>{p}</Select.Option>
+						})}
+					</Select>
+				</Form.Item>
+				<Form.Item name={'apiKey'} label="Api Key">
+					<Input />
+				</Form.Item>
+				<Form.Item name={'secret'} label="Secret">
+					<Input />
+				</Form.Item>
+				<Form.Item name={'password'} label="Password">
+					<Input />
+				</Form.Item>
+			</Form>
 		</Modal>
 	)
 }
